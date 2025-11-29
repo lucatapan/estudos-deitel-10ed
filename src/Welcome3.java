9ee3f57887d33957e06d9de10b2099a0904d0d27
LucianoCatapan <lcatapan@gmail.com>
2025-11-28 23:03:22 -0300

    "terceiro commit estudos deitel 10a edição via intellij"

---
+++ src/Welcome3.java
@@ -0,0 +1,10 @@
+//escrevendo multiplas linhas de texto com uma única instrução
+
+        public class Welcome3 {
+
+        public static void main(String[] args){
+
+            System.out.println("Bem-vindo\naos\nestudos\nde JAVA por Luciano lendo Deitel");
+
+        }
+}
