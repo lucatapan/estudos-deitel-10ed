a4bc2ce2fcfea3d54f2a55f132eafc0adf097238
LucianoCatapan <lcatapan@gmail.com>
2025-11-28 22:29:18 -0300

    "Primeiro commit estudos deitel 10a edição via intellij"

---
+++ src/Welcome1.java
@@ -0,0 +1,10 @@
+/* Primeiro código do livro Deitel 10ª edição Welcome1.java @LucianoCatapan 28/11/25*/
+
+public class Welcome1 {
+
+    /* metodo main inicia a execução do aplicato */
+
+    static void main(String[] args) {
+        System.out.println("Programa hello world Welcome1 de Luciano Catapan!");
+    }//fim do main
+}//fim da classe
