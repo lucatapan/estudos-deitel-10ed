77a24edb32e5fa09c96225789d12dc07f049e5f3
LucianoCatapan <lcatapan@gmail.com>
2025-11-28 22:55:35 -0300

    "segundo commit estudos deitel 10a edição via intellij"

--- src/Welcome2.java
+++ src/Welcome2.java
@@ -4,7 +4,7 @@ public class Welcome2  {
 
     public static void main(String[] args){
 
-        System.out.print("Bem-vindo");
+        System.out.print("Bem-vindo ");
         System.out.println("aos Estudos Deitel Java");
 
     }
