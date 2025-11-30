cd59d90c573d3d4f577179064346347484be5791
LucianoCatapan <lcatapan@gmail.com>
2025-11-30 12:02:46 -0300

    "quarto commit estudos deitel 10a edição via intellij"

---
+++ src/Welcome4.java
@@ -0,0 +1,13 @@
+//exibindo multiplas linhas com o metodo System.out.prinf
+
+
+public class Welcome4 {
+
+    public static void main(String[] args){
+
+        System.out.printf("%s%n%s%n",
+                "Bem-vindo ", "Aos estudos Java Deitel Luciano");
+
+    }
+
+}
