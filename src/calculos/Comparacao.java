48bf2a8dde871f02f224736bc72817946b64917c
LucianoCatapan <lcatapan@gmail.com>
2025-12-01 16:37:08 -0300

    "setimo commit estudos deitel 10a edição via intellij"

--- src/calculos/Comparacao.java
+++ src/calculos/Comparacao.java
@@ -32,7 +32,7 @@ public class Comparacao {
             if (numero1 <= numero2){
                 System.out.printf("%d <= %d%n", numero1, numero2);
             }
-            if (numero1 => numero2){
+            if (numero1 >= numero2){
                 System.out.printf("%d => %d%n", numero1, numero2);
             }
 
